9f1cdfaf4ddc97e720d02de9d562ce2bd5453ec1
UdayByreddy <139316540+UdayByreddy@users.noreply.github.com>
2026-04-30 12:03:49 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int maxProfit(int k, int[] prices) {
+            int n = prices.length;
+        int[][][] dp = new int[n][2][3];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<2;j++){
+                Arrays.fill(dp[i][j],-1);
+            }
+        }
+        return getAns(prices,0,0,k,dp,n);
+    }
+        public int getAns(int[] prices,int index,int buy,int k,int[][][] dp,int n){
+        if(index==n || k==0) return 0;
+        if(dp[index][buy][k]!=-1) return dp[index][buy][k];
+        int profit=0;
+        if(buy==0 ){
+            profit = Math.max((0+getAns(prices,index+1,0,k,dp,n)),(-prices[index]+getAns(prices,index+1,1,k,dp,n)));
+        }
+        else{
+            profit = Math.max((0+getAns(prices,index+1,1,k,dp,n)),(prices[index]+getAns(prices,index+1,0,k-1,dp,n)));
+        }
+         dp[index][buy][k]=profit;
+         return profit;
+    }
+}
